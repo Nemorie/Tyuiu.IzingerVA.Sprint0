ab8830611a0d9b11b168725a5d4838c9f1ca1957
Nemorie <nemiroeee@gmail.com>
2026-09-10 00:10:42 +0500

    Task4

---
+++ Tyuiu.IzingerVA.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace Tyuiu.IzingerVA.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(in int a, in int b)
+        {
+            return a / b;
+        }
+    }
+}
