cb2e2963b5734cf7700fd6d71ec7010a1c1cbbdb
Nemorie <nemiroeee@gmail.com>
2026-09-10 01:49:08 +0500

    Task7

---
+++ Tyuiu.IzingerVA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.IzingerVA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArrays = new int[5];
+            for (var i = 0; i < resultArrays.Length; i++)
+            {
+                resultArrays[i] = numOne[i] + numTwo[i];
+            }
+            return resultArrays;
+        }
+
+    }
+}
---
+++ Tyuiu.IzingerVA.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+    using Tyuiu.IzingerVA.Sprint0.Task7.V0.Lib;
+    namespace Tyuiu.IzingerVA.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            int[] nums1 = { 1, 1, 1, 1, 1 };
+            int[] nums2 = { 1, 1, 1, 1, 1 };
+            int[] nums3 = { 2, 2, 2, 2, 2 };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+        }
+    }
+}
