c3e7087882900ea356a5ceff5f70ca70a642a8c1
Nemorie <nemiroeee@gmail.com>
2026-09-10 01:07:49 +0500

    Task6

---
+++ Tyuiu.IzingerVA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+    using Tyuiu.IzingerVA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.IzingerVA.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
