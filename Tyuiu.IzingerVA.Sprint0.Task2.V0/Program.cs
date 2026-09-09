2dc2f5eded682c9e5617d7e8c27d03333140f238
Nemorie <nemiroeee@gmail.com>
2026-09-09 23:02:41 +0500

    Task2

---
+++ Tyuiu.IzingerVA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tyuiu.IzingerVA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.IzingerVA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Влад"));
+            Console.ReadKey();
+        }
+    }
+}
