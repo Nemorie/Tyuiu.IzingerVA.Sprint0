571daea4755e983d206d37964e3ab9243e5c7cc8
Nemorie <nemiroeee@gmail.com>
2026-09-09 23:27:43 +0500

    Task3

---
+++ Tyuiu.IzingerVA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tyuiu.IzingerVA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.IzingerVA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadLine();
+        }
+    }
+}
